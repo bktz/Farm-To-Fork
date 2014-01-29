d60db43f3e929552c0118451fe35a08b69e50f01
jtempelm@gmail.com <jtempelm@gmail.com>
2014-01-29 10:19:49 -0500

    Modified the farm to fork style modified composer.json with updated header information

--- app/views/site/blog.blade.php
+++ app/views/site/blog.blade.php
@@ -4,6 +4,7 @@
 {{-- Content --}}
 @section('content')
 
-<h2>blog page</h2>
+<h1>Farm To Fork Newsfeed</h1>
+<p>Keep up with Farm To Fork through our <a href="http://farmtoforkguelph.wordpress.com">blog</a>, on <a href ="http://www.facebook.com/farmtoforkguelph">facebook</a> or on <a href="http://www.twitter.com/Farm_2_Fork">twitter</a></p>
 
 @stop
--- app/views/site/index.blade.php
+++ app/views/site/index.blade.php
@@ -4,4 +4,8 @@
 @section('content')
 <h1>Farm-To-Fork!</h1>
 <p>Eliminating food insecurity in our community.</p>
+
+<h2>Welcome!</h2>
+The goal of Farm To Fork is to radically change the way we think about food insecurity in our communities. We believe that all of us, working together, 
+can dramatically improve the quality and quantity of food available to those who need it most. You can help.
 @stop
