3c8de72ab5b34ebe10087c5650079f9e5d0ac333
Erica Pisani <pisani.erica@gmail.com>
2014-03-30 14:44:13 -0400

    Improve readability in routes.php.

--- app/routes.php
+++ app/routes.php
@@ -27,28 +27,38 @@ Route::group(array('prefix' => 'admin', 'before' => 'auth'), function()
 {
 	# User Management
 	Route::get('users/{user}/show', 'AdminUsersController@getShow')
-	->where('user', '[0-9]+');
+		->where('user', '[0-9]+');
+
 	Route::get('users/{user}/edit', 'AdminUsersController@getEdit')
-	->where('user', '[0-9]+');
+		->where('user', '[0-9]+');
+
 	Route::post('users/{user}/edit', 'AdminUsersController@postEdit')
-	->where('user', '[0-9]+');
+		->where('user', '[0-9]+');
+
 	Route::get('users/{user}/delete', 'AdminUsersController@getDelete')
-	->where('user', '[0-9]+');
+		->where('user', '[0-9]+');
+
 	Route::post('users/{user}/delete', 'AdminUsersController@postDelete')
-	->where('user', '[0-9]+');
+		->where('user', '[0-9]+');
+
 	Route::controller('users', 'AdminUsersController');
 
 	# User Role Management
 	Route::get('roles/{role}/show', 'AdminRolesController@getShow')
-	->where('role', '[0-9]+');
+		->where('role', '[0-9]+');
+
 	Route::get('roles/{role}/edit', 'AdminRolesController@getEdit')
-	->where('role', '[0-9]+');
+		->where('role', '[0-9]+');
+
 	Route::post('roles/{role}/edit', 'AdminRolesController@postEdit')
-	->where('role', '[0-9]+');
+		->where('role', '[0-9]+');
+
 	Route::get('roles/{role}/delete', 'AdminRolesController@getDelete')
-	->where('role', '[0-9]+');
+		->where('role', '[0-9]+');
+
 	Route::post('roles/{role}/delete', 'AdminRolesController@postDelete')
-	->where('role', '[0-9]+');
+		->where('role', '[0-9]+');
+
 	Route::controller('roles', 'AdminRolesController');
 
 	# Admin Dashboard
@@ -62,13 +72,15 @@ Route::group(array('prefix' => 'admin', 'before' => 'auth'), function()
 
 // User reset routes
 Route::get('user/reset/{token}', 'UserController@getReset')
-->where('token', '[0-9a-z]+');
+	->where('token', '[0-9a-z]+');
+
 // User password reset
 Route::post('user/reset/{token}', 'UserController@postReset')
-->where('token', '[0-9a-z]+');
+	->where('token', '[0-9a-z]+');
+
 //:: User Account Routes ::
 Route::post('user/{user}/edit', 'UserController@postEdit')
-->where('user', '[0-9]+');
+	->where('user', '[0-9]+');
 
 //:: User Account Routes ::
 Route::post('user/login', 'UserController@postLogin');
@@ -122,7 +134,7 @@ Route::get('license', function (){
 Route::get('contact-us', function (){
 	return View::make('site/contact-us');
 });
-	
+
 
 # Index Page - Last route, no matches
 Route::get('/', function (){
